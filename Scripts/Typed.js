69e8861d090324a013b5dd95737771e3c6560b09
jordanrafaell <jordanrafaelflorencio@gmail.com>
2023-10-15 11:00:21 -0300

    refactor: pages/css

--- Scripts/Typed.js
+++ Scripts/Typed.js
@@ -18,23 +18,3 @@ function removerTyped() {
 
 document.addEventListener("DOMContentLoaded", iniciarTyped); // Inicialize o Typed.js no carregamento da página
 
-// Verifique a largura da tela quando a página é carregada
-window.addEventListener("load", function () {
-  var larguraDaTela = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-
-  if (larguraDaTela < 900) {
-    removerTyped(); // Remova o Typed.js se a largura da tela for menor que 900 pixels
-  }
-});
-
-// Adicione um ouvinte de evento de redimensionamento para verificar a largura da tela
-window.addEventListener('resize', function() {
-  var larguraDaTela = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-
-  // Verifique se a largura da tela é menor que 900 pixels
-  if (larguraDaTela < 900) {
-    removerTyped(); // Remova o Typed.js
-  } else {
-    iniciarTyped(); // Inicialize o Typed.js novamente
-  }
-});
