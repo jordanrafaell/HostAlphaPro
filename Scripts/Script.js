c7c7989b56b50d7e646f590380a307eee733c716
jordanrafaell <jordanrafaelflorencio@gmail.com>
2023-10-19 17:33:15 -0300

    Projeto finalizado

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -3,9 +3,25 @@ document.addEventListener("DOMContentLoaded", function(){
     const btnMobile = document.querySelector(".btn_mobile");
     const Mobile = document.querySelector(".list_mobile");
 
+    Mobile.classList.remove("active")
+
     btnMobile.addEventListener("click", () =>{
 
         Mobile.classList.toggle("active");
 
     });
 });
+
+// nav Scroll box-shadow
+window.addEventListener("scroll", function() {
+    // Altura em que você deseja ativar o box-shadow
+    var scrollHeight = 100;
+  
+    if (window.scrollY >= scrollHeight) {
+      document.getElementById("elementToApplyBoxShadow").style.boxShadow = " 0 10px 20px #000";
+
+    } else {
+      document.getElementById("elementToApplyBoxShadow").style.boxShadow = "none";
+    }
+  });
+  
--- Scripts/Typed.js
+++ Scripts/Typed.js
@@ -4,7 +4,7 @@ function iniciarTyped() {
   if (typed === null) {
     typed = new Typed('.home_text_custom', {
       strings: ["O melhor serviço de hospedagem na web para o seu site."],
-      typeSpeed: 80,
+      typeSpeed: 60,
     });
   }
 }
@@ -18,3 +18,23 @@ function removerTyped() {
 
 document.addEventListener("DOMContentLoaded", iniciarTyped); // Inicialize o Typed.js no carregamento da página
 
+// Verifique a largura da tela quando a página é carregada
+window.addEventListener("load", function () {
+  var larguraDaTela = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+  if (larguraDaTela < 900) {
+    removerTyped(); // Remova o Typed.js se a largura da tela for menor que 900 pixels
+  }
+});
+
+// Adicione um ouvinte de evento de redimensionamento para verificar a largura da tela
+window.addEventListener('resize', function() {
+  var larguraDaTela = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+  // Verifique se a largura da tela é menor que 900 pixels
+  if (larguraDaTela < 900) {
+    removerTyped(); // Remova o Typed.js
+  } else {
+    iniciarTyped(); // Inicialize o Typed.js novamente
+  }
+});
